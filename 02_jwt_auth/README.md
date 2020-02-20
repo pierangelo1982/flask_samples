c43f71d5409ffd77c8fd1945ac3b137f6bbb7715
pierangelo1982 <pierangelo1982@gmail.com>
2020-02-20 21:30:01 +0100

    basic jwt app

--- 02_jwt_auth/README.md
+++ 02_jwt_auth/README.md
@@ -11,3 +11,23 @@ install requirements:
 ```
 pip3 install -r requirements.txt
 ```
+
+http://127.0.0.1:5000/home
+```
+{
+  "message": "tutti mi possono vedere"
+}
+```
+
+http://127.0.0.1:5000/login
+```
+{
+  "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoidXNlciIsImV4cCI6MTU4MjIzMjMwOH0.yaG71sw-Ldb47juRniChaZUKyu1zxVEtxjzhLmoZ4I8"
+}
+```
+http://127.0.0.1:5000/dashboard?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoidXNlciIsImV4cCI6MTU4MjIzMjMwOH0.yaG71sw-Ldb47juRniChaZUKyu1zxVEtxjzhLmoZ4I8
+```
+{
+  "message": "area riservata"
+}
+```
--- 02_jwt_auth/app.py
+++ 02_jwt_auth/app.py
@@ -1,18 +1,51 @@
 from flask import Flask, jsonify, request, make_response
+import jwt
+import datetime
+from functools import wraps
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'sopralapancalacapracanta'
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.args.get('token') # http://127.0.0.1:5000/route?token=kjfkjgkfjgkfdjgkdf
+        if not token:
+            return jsonify({'message' : 'Token non presente'}), 403
+
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+        
+        except:
+            return jsonify({'message' : 'Token non valido'}), 403
+        
+        return f(*args, **kwargs)
+    
+    return decorated
+
+
 @app.route('/home')
 def home():
-    return ''
+    return jsonify({'message' : 'tutti mi possono vedere'})
+
 
 @app.route('/dashboard')
+@token_required
 def dashboard():
-    return ''
+    return jsonify({'message' : 'area riservata'})
+
 
 @app.route('/login')
 def login():
-    return ''
+    auth = request.authorization
+
+    if auth and auth.password == 'password':
+        token = jwt.encode({'user' : auth.username, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+        return jsonify({'token' : token.decode('UTF-8')})
+
+    return make_response('Non posso verificare la passord', 401, {'WWW-Authenticate' : 'Basic real="Password Required"'})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
